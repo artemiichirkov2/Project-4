0fc0c91938763e5699007f8c9e933cd30e1b99a0
mahesh2002 <37019736+mahesh2002@users.noreply.github.com>
2022-04-11 16:33:26 -0400

    Account updates are now stored in text files

---
+++ newestMahesh/Student.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Student {
+        private String firstName;
+        private String lastName;
+        private String username;
+        private String password;
+        private ArrayList<Integer> enrolledCourses;
+
+        public Student(String firstName, String lastName, String username, String password) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.username = username;
+            this.password = password;
+            this.enrolledCourses = new ArrayList<Integer>();
+        }
+
+    public ArrayList<Integer> getEnrolledCourses() {
+        return enrolledCourses;
+    }
+
+    public void setEnrolledCourses(ArrayList<Integer> enrolledCourses) {
+        this.enrolledCourses = enrolledCourses;
+    }
+
+    public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+//    public static void main(String[] args) throws IOException {
+//
+//        Scanner s = new Scanner(System.in);
+//        Student[]students = students.scanner.nextLine();
+//        //Student credentials, does it match up with old account, if so, continue. If not, reenter
+//    }
+
+}
